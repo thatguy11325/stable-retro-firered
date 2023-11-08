2d26a56fc85cefa27dbedd5a29eadcac0b475afb
thatguy11325 <148832074+thatguy11325@users.noreply.github.com>
2023-11-07 21:08:59 -0500

    Add Fire Red Demo

---
+++ pokemon.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+import retro
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def main():
+  retro.data.Integrations.add_custom_path(SCRIPT_DIR)
+  print("FireRed exists: ", "FireRed-GBAdvance" in retro.data.list_games(inttype=retro.data.Integrations.ALL))
+  env = retro.make("FireRed-GBAdvance", inttype=retro.data.Integrations.ALL, render_mode="rgb_array")
+  env.reset()
+
+  n_steps = 1000
+
+  start = time.time()
+  for _ in range(n_steps):
+    x = env.step(env.action_space.sample())
+    env.render()
+  env.close()
+  end = time.time()
+
+  print(f"Stable Retro ran {n_steps} in {end-start} seconds or {n_steps / (end-start)} steps/second")
+
+if __name__ == "__main__":
+  main()
